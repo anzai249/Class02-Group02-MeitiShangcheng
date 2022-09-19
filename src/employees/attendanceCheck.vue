8b336cd9fa62465847a12f1ea64ea72fab084090
yinxulong <774226046@qq.com>
2022-09-19 22:00:10 +0800

    Add a check button in attendanceCheck

--- src/employees/attendanceCheck.vue
+++ src/employees/attendanceCheck.vue
@@ -2,11 +2,15 @@
 body {
     background-color: black;
 }
+#btn {
+    width: 50px;height: 50px;border-radius: 50%;border: none
+}
 </style>
 <template>
     <div>
         <h1>-Your Work Shift-</h1><br />
         <h2>{{userid}}</h2>
+        <input type="button" value="Check" id="btn">
     </div>
 </template>
 <script>
